827d6234fc9b8bfee77515a544b23888dfbe869f
nachomirsol <nachomirsol@gmail.com>
2018-07-30 11:21:22 +0200

    directory structure and first test endpoints for users, profile and posts, server connection to db

---
+++ config/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://nachal:123456a@ds233238.mlab.com:33238/develapp"
+};
---
+++ routes/api/users.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// @route GET api/users/test
+// @desc Test users route
+// @access Private
+router.get("/test", (req, res) => {
+  res.json({ message: "users works" });
+});
+
+module.exports = router;
