b58510cc65577cb2891d75a57e4e8486f15dd98b
nachomirsol <nachomirsol@gmail.com>
2018-08-07 11:01:44 +0200

    react router implementation, login and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
-import Navbar from './components/layout/Navbar'
-import Landing from './components/layout/Landing'
-import Footer from './components/layout/Footer'
+
+import { BrowserRouter, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Footer from "./components/layout/Footer";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component = {Register}/>
+            <Route exact path="/login" component = {Login}/>
+          </div>
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
