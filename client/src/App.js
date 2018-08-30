7c5565ce5db9a9abc4d80e20ecbc383e39129427
nachomirsol <nachomirsol@gmail.com>
2018-08-30 09:20:33 +0200

    profile action and reducer

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import {setCurrentUser} from './actions/authActions';
+import {setCurrentUser,logoutUser} from './actions/authActions';
+import {clearCurrentProfile} from './actions/profileActions';
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -12,6 +13,7 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Footer from "./components/layout/Footer";
+import Dashboard from "./components/dashboard/Dashboard";
 import "./App.css";
 
 // Check for token
@@ -22,6 +24,18 @@ if(localStorage.jwtToken){
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+  
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if(decoded.exp < currentTime){
+    //logout user
+    store.dispatch(logoutUser());
+    // Clear current profile
+    store.dispatch(clearCurrentProfile());
+
+    // Redirect to login
+    window.location.href = '/login';
+  }
 }
 
 class App extends Component {
@@ -35,6 +49,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/DASHBOARD" component={Dashboard} />
             </div>
             <Footer />
           </div>
