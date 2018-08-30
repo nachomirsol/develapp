87db0adf5e262d6ba5fc7c5e55de18c789b7ab5c
nachomirsol <nachomirsol@gmail.com>
2018-08-30 08:26:18 +0200

    login and navbar

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -62,23 +62,23 @@ class Login extends Component{
                   <h1 className="display-4 text-center">Log In</h1>
                   <p className="lead text-center">Sign in to your DevConnector account</p>
                   <form onSubmit = {this.onSubmit}>
-                  <TextFieldGroup
-                    placeholder="Email Address"
-                    value = {this.state.email}
-                    type="email"
-                    name="email" 
-                    onChange = {this.onChange}
-                    error = {errors.email}     
-                  />
-
                     <TextFieldGroup
-                        placeholder="Password"
-                        value = {this.state.password}
-                        type="password"
-                        name="password" 
+                        placeholder="Email Address"
+                        value = {this.state.email}
+                        type="email"
+                        name="email" 
                         onChange = {this.onChange}
-                        error = {errors.password}     
+                        error = {errors.email}     
                     />
+
+                        <TextFieldGroup
+                            placeholder="Password"
+                            value = {this.state.password}
+                            type="password"
+                            name="password" 
+                            onChange = {this.onChange}
+                            error = {errors.password}     
+                        />
                     <input type="submit" className="btn btn-info btn-block mt-4" />
                   </form>
                 </div>
